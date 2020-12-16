6b547a289391ec9f37bc71efab9c5d6e03d1cec8
lemon-lexiaobai <17743692127@163.com>
2020-12-16 23:13:12 +0800

    图书管理页面

--- src/components/book/book.vue
+++ src/components/book/book.vue
@@ -5,10 +5,15 @@
       style="border: 1px solid rgb(235, 237, 240)"
     />
     <a-col :span="16" :offset="4">
-      <a-table :columns="columns" :data-source="book">
+      <a-table :rowKey="record => record.id" :columns="columns" :data-source="book">
         <span slot="category" slot-scope="category">
           <a-tag color="green">
             {{cate(category)}}
+          </a-tag>
+        </span>
+        <span slot="state" slot-scope="state">
+          <a-tag :color="getState(state).color">
+            {{getState(state).desc}}
           </a-tag>
         </span>
       </a-table>
@@ -21,25 +26,41 @@ import { getBook, getCategory } from 'common/js/cache'
 
 const columns = [
   {
-    title: 'id',
+    title: '图书编号',
     dataIndex: 'id'
   },
   {
-    title: 'name',
+    title: '图书名称',
     dataIndex: 'name'
   },
   {
-    title: 'press',
+    title: '图书出版社',
     dataIndex: 'press'
   },
   {
-    title: 'category',
+    title: '图书分类',
     dataIndex: 'category',
     scopedSlots: { customRender: 'category' }
   },
   {
-    title: 'state',
-    dataIndex: 'state'
+    title: '图书状态',
+    dataIndex: 'state',
+    scopedSlots: { customRender: 'state' }
+  }
+]
+
+const staRela = [
+  {
+    desc: '已借出',
+    color: 'geekblue'
+  },
+  {
+    desc: '在库',
+    color: 'green'
+  },
+  {
+    desc: '已灭失',
+    color: 'volcano'
   }
 ]
 
@@ -66,6 +87,9 @@ export default {
     cate(cateID) {
       let index = this.categories.findIndex(item => item.id === cateID)
       return this.categories[index].name
+    },
+    getState(state) {
+      return staRela[state]
     }
   }
 }
