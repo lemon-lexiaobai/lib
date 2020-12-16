f3595dd78d682ba1a5ac0f5471afcbcdd19d41b3
lemon-lexiaobai <17743692127@163.com>
2020-12-16 23:23:59 +0800

    缓存相关

--- src/common/js/cache.js
+++ src/common/js/cache.js
@@ -13,10 +13,41 @@ export function insertBook(book) {
   storage.set(BOOK_KEY, arr)
 }
 
-export function deleteBook() {}
+export function bookNotBorrow() {
+  let arr = getBook()
+  return arr.filter(item => item.state === 1)
+}
+
+export function updateBook(id, state) {
+  let arr = getBook()
+  let index = arr.findIndex(item => item.id === String(id))
+  if (index >= 0) {
+    let book = arr[index]
+    book.state = state
+    arr.splice(index, 1, book)
+  }
+  storage.set(BOOK_KEY, arr)
+}
 
 export function getBorrow() {
   return storage.get(BORROW_KEY, [])
+}
+
+export function insertBorrow(info) {
+  let arr = getBorrow()
+  arr.push(info)
+  storage.set(BORROW_KEY, arr)
+}
+
+export function updateBorrow(personId, bookId, state) {
+  let arr = getBorrow()
+  let index = arr.findIndex(item => item.bookId === bookId && item.personId === personId)
+  arr[index].state = state
+  // storage(BORROW_KEY, arr)
+}
+
+export function clearBorrow() {
+  storage.set(BORROW_KEY, [])
 }
 
 export function getCategory() {
